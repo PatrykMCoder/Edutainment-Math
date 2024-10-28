dfcc7556854265cd2e2942c94be221e3b43fbab4
PatrykMCoder <patrykmarciszewski3@gmail.com>
2024-10-28 18:52:20 +0100

    Initial Commit

---
+++ Edutainment Math/Edutainment_MathApp.swift
@@ -0,0 +1,17 @@
+//
+//  Edutainment_MathApp.swift
+//  Edutainment Math
+//
+//  Created by Patryk Marciszewski on 28/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Edutainment_MathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Edutainment MathTests/Edutainment_MathTests.swift
@@ -0,0 +1,17 @@
+//
+//  Edutainment_MathTests.swift
+//  Edutainment MathTests
+//
+//  Created by Patryk Marciszewski on 28/10/2024.
+//
+
+import Testing
+@testable import Edutainment_Math
+
+struct Edutainment_MathTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
